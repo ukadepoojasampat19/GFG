20cf64160e51030a82764e16bd8703146e5dfdd4
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-03-10 16:28:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    long maximumSumSubarray(int K, vector<int> &Arr , int N){
+        int i=0;
+        int j=0;
+        
+        long sum=0;
+        long max_ele=INT_MIN;
+        while(j < N)
+        {
+            sum+=Arr[j];
+            if((j-i+1) < K)
+            {
+                j++;
+            }
+            else
+            {
+                max_ele=max(max_ele,sum);
+                sum-=Arr[i];
+                i++;
+                j++;
+            }
+        }
+        return max_ele;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,K;
+        cin >> N >> K;;
+        vector<int>Arr;
+        for(int i=0;i<N;++i){
+            int x;
+            cin>>x;
+            Arr.push_back(x);
+        }
+        Solution ob;
+        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
