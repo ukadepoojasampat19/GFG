4ffa2c561554211d2231c2479b83c9cb1b36687c
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-07-31 11:44:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Coin Change/coin-change.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long int count(int coins[], int N, int sum) {
+        vector<vector<long long int>> dp(N + 1, vector<long long int>(sum + 1, 0));
+    
+    // Initialize the base case
+    for (int i = 0; i <= N; i++) {
+        dp[i][0] = 1;
+    }
+    
+    // Fill the dp table
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= sum; j++) {
+            if (coins[i-1] <= j) {
+                dp[i][j] = dp[i-1][j] + dp[i][j - coins[i-1]];
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    
+    return dp[N][sum];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int sum, N;
+        cin >> sum >> N;
+        int coins[N];
+        for (int i = 0; i < N; i++) cin >> coins[i];
+        Solution ob;
+        cout << ob.count(coins, N, sum) << endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
