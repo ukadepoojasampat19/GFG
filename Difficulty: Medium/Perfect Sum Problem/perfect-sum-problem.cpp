e5d5d23927a61782712307b014680f36915527a4
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-08-03 21:45:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -6,27 +6,47 @@ using namespace std;
 class Solution{
 
 	public:
-int mod = 1e9 + 7;
-
-int sub(int arr[], int sum, int n, int idx, int currSum, vector<vector<int>>& dp) {
-    if (idx == n) {
-        return (currSum == sum) ? 1 : 0;
-    }
-    
-    if (currSum > sum) return 0;
-    
-    if (dp[idx][currSum] != -1) return dp[idx][currSum];
-    
-    int take = sub(arr, sum, n, idx + 1, currSum + arr[idx], dp);
-    int notTake = sub(arr, sum, n, idx + 1, currSum, dp);
-    
-    return dp[idx][currSum] = (take + notTake) % mod;
-}
-
-int perfectSum(int arr[], int n, int sum) {
-    vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
-    return sub(arr, sum, n, 0, 0, dp);
-}
+	int mod = 1e9 +7;
+	/*int fun(int arr[],int n,int sum,int idx,int curr_sum,vector<vector<int>>& dp)
+	{
+	    if(idx == n)
+	    {
+	        return ( curr_sum == sum)? 1:0;
+	    }
+	    if(curr_sum > sum) return 0;
+	    
+	    if(dp[idx][curr_sum] != -1) return dp[idx][curr_sum];
+	    int taken = fun(arr,n,sum,idx+1,curr_sum + arr[idx],dp);
+	    int notaken = fun(arr,n,sum,idx+1,curr_sum,dp);
+	    
+	    return dp[idx][curr_sum] = (taken+notaken) % mod;
+	}*/
+	int perfectSum(int arr[], int n, int sum)
+	{
+	    /*vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
+        return fun(arr,n,sum,0,0,dp);*/
+        
+        vector<vector<int>> dp(n+1,vector<int>(sum+1,0));
+        
+        dp[0][0] =1;
+        
+        for(int i=1;i<=n;i++)
+        {
+            
+            for(int j=0;j<=sum;j++)
+            {
+                //not include 
+                dp[i][j] = dp[i-1][j];
+                
+                //include the element
+                if(arr[i-1] <= j)
+                {
+                    dp[i][j] = (dp[i][j] + dp[i-1][j - arr[i-1]]) % mod;
+                }
+            }
+        }
+        return dp[n][sum];
+	}
 	  
 };
 
