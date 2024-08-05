d5ca3961f2a5c0eb342acc956fb8376f038b7fca
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2024-08-05 12:57:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -6,29 +6,27 @@ using namespace std;
 
 // } Driver Code Ends
 //User function template for C++
- 
+
 class Solution{   
 public:
-    int count_sum(int idx,int k,vector<int> arr,int sum,vector<vector<int>>& dp)
+    bool idExists(int idx,int k,vector<int>arr,int sum,vector<vector<int>> & dp)
     {
-        if( idx == arr.size())
-        {
-            return (k == sum)? 1: 0;
-        }
+        if( sum == k) return true;
         
-        //if k > sum
-        if( k > sum) return 0;
+        if( k > sum || idx == arr.size()) return false;
         
         if(dp[idx][k] != -1) return dp[idx][k];
-        int l = count_sum(idx+1,k + arr[idx],arr,sum,dp);
-        int r = count_sum(idx+1,k,arr,sum,dp);
         
-        return dp[idx][k] = l+r;
+        bool include = idExists(idx+1,k + arr[idx],arr,sum,dp);
+        bool exclude = idExists(idx+1,k,arr,sum,dp);
+        
+        
+        dp[idx][k] = include || exclude;
+        return dp[idx][k];
     }
     bool isSubsetSum(vector<int>arr, int sum){
-        
-        vector<vector<int>> dp(arr.size()+1, vector<int>(sum+1, -1));
-       return count_sum(0,0,arr,sum,dp);
+        vector<vector<int>> dp(arr.size()+1,vector<int>(sum+1,-1));
+        return idExists(0,0,arr,sum,dp);
     }
 };
 
